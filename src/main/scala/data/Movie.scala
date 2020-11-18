e8954c2efac53157c57906d2de13e7a7927f4a53
Mateusz Rusnaczyk <mrusna4@uic.edu>
2020-11-18 12:32:34 -0600

    add toString method to Movie class

--- src/main/scala/data/Movie.scala
+++ src/main/scala/data/Movie.scala
@@ -6,4 +6,12 @@ package data
   * @param year
   * @param revenue
   */
-class Movie(title: String, year: Int, revenue: Double){}
+class Movie(title: String, year: Int, revenue: Double) {
+  /**
+    * Returns the serialized Movie data in a easy-to-read format
+    * @return
+    */
+  override def toString(): String = {
+   return s"(title='${title}', year=${year}, revenue=${revenue})"
+  }
+}
