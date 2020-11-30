9d9cfd799fd9fa4f0000020edeeb2d041b4fad04
Mateusz Rusnaczyk <mrusna4@uic.edu>
2020-11-30 10:36:19 -0600

    add methods to check if a point is in range of a DimensionRange and a Zone

--- src/main/scala/can/util/DimensionRange.scala
+++ src/main/scala/can/util/DimensionRange.scala
@@ -5,4 +5,12 @@ package can.util
   * @param from start of the range, inclusive,
   * @param to end of the range, exclusive
   */
-class DimensionRange(var from: Int, var to: Int) {}
+class DimensionRange(var from: Int, var to: Int) {
+
+  /**
+    * Checks if a point is within the range [from, to).
+    * @param point Value to check if in range.
+    * @return True if `point` is in range, false otherwise.
+    */
+  def isPointInRange(point: Int): Boolean = (point >= from && point < to)
+}
--- src/main/scala/can/util/Zone.scala
+++ src/main/scala/can/util/Zone.scala
@@ -8,4 +8,24 @@ package can.util
 class Zone (zoneRange: List[DimensionRange], d: Int) {
   // Check that the dimension ranges that were passed match the number of dimensions in the simulation
   assert(zoneRange.length == d)
+
+  /**
+    * Checks if a point is within the zone
+    * @param point
+    * @return
+    */
+  def isPointInZone(point: List[Int]): Boolean = {
+    // Assert that the point we are given is d-dimensional
+    assert(point.length == d)
+
+    // Check if the the point is in range for all dimensions.
+    point
+        .zip(zoneRange)
+        .forall(entry => {
+          val pointOnDimension = entry._1
+          val dimensionRange = entry._2
+
+          dimensionRange.isPointInRange(pointOnDimension)
+        })
+  }
 }
